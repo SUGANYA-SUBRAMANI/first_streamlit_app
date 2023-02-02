98a58db119fb8b820ad70784045eb9cb3858d2ee
SUGANYA-SUBRAMANI <124239959+SUGANYA-SUBRAMANI@users.noreply.github.com>
2023-02-02 15:10:37 +0530

    Update streamlit_app.py
    added txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('my parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 and blueberry oatmeal')
+streamlit.text('kale ,spinach & rocket smoothie')
